2090953dcafb820df3e70dcbb5fcee48f8d3e8b5
Leonardo <leo.dechi99@gmail.com>
2022-07-16 23:26:47 +0200

    added a comment

--- server/utils/format-user.ts
+++ server/utils/format-user.ts
@@ -1,6 +1,11 @@
 import { Types } from 'mongoose';
 import { User } from '../models/User';
 
+/**
+ * Return only some fields of the user object.
+ * @param user the user object
+ * @returns the formatted user object
+ */
 export const formatUser = (
   user: User
 ): {
